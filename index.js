8618b3100c8204bbdb1ce5f2cf0ea43d12bfe8ef
kajal gupta <kajalg1401@example.com>
2024-05-03 22:03:28 +0530

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const authController = require('./controllers/authController')
 const blogController = require('./controllers/blogController')
 const multer = require('multer')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT 
 //connect db
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URI)
